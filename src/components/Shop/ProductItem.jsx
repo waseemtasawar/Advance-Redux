da7b4ebd0cec139a8da09ba246d407beec4dba95
waseem tasawar <waseemtasawar97@gmail.com>
2024-10-14 17:59:31 +0500

    update Ui

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -1,18 +1,51 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 import { cartActions } from "../store/cart-slice";
 const ProductItem = (props) => {
   const { title, price, description, id } = props;
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-  const handleAddToCart = () => {
-    dispatch(
-      cartActions.additemToCart({
-        id,
-        title,
-        price,
-      })
-    );
+  const addToCartHandler = () => {
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+      updatedItem.quantity++;
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id,
+    //     title,
+    //     price,
+    //   })
+    // );
   };
 
   return (
@@ -24,7 +57,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button onClick={handleAddToCart}>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -7,6 +7,10 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     additemToCart: (state, action) => {
       const newItem = action.payload;
       const exestingItem = state.items.find((item) => item.id === newItem.id);
